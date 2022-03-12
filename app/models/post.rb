7e565e3a05385aa26bbb6331cd80a1d7f0da1c99
Oluwafemi Awoyemi <femikola23@gmail.com>
2022-03-12 15:59:49 +0400

    Add validation methods to model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
+  validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :title, presence: true, length: { maximum: 250 }
+
   belongs_to :user
   has_many :comments
   has_many :likes
