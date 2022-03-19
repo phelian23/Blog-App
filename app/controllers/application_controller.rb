a8c742610b5b21cda68dcab1a93010adbf4dea34
Oluwafemi Awoyemi <femikola23@gmail.com>
2022-03-19 23:33:31 +0400

    Add Api end point

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery prepend: true
+  before_action :authenticate_user!, unless: :api_path
 
   before_action :update_allowed_parameters, if: :devise_controller?
 
@@ -7,10 +8,14 @@ class ApplicationController < ActionController::Base
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:name, :posts_counter, :email, :password, :password_confirmation)
+      u.permit(:name, :email, :password, :password_confirmation)
     end
     devise_parameter_sanitizer.permit(:account_update) do |u|
       u.permit(:name, :surname, :email, :password, :current_password)
     end
+  end
+
+  def api_path
+    request.original_url.include?('api')
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_17_081108) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_19_192742) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,9 +53,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_17_081108) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "posts_counter"
+    t.integer "posts_counter", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "api_token"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
